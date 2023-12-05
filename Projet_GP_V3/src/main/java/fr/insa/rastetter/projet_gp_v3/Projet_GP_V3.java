7063595fc31d82081154d05551aeecb67f82b4ff
arnaud <arnaud@XPS-13-Arnaud>
2023-12-05 19:27:37 +0100

    Merge origin/main into main
    Conflicts:
    	src/main/java/fr/insa/rastetter/mainview/Entete.java
    	src/main/java/fr/insa/rastetter/mainview/FenetrePartagee.java
    	src/main/java/fr/insa/rastetter/mainview/VuePrincipale.java

---
+++ Projet_GP_V3/src/main/java/fr/insa/rastetter/projet_gp_v3/Projet_GP_V3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package fr.insa.rastetter.projet_gp_v3;
+
+/**
+ *
+ * @author arnaud
+ */
+public class Projet_GP_V3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
